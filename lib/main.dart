f80ce97b5605988b149dc8d74bc087cf51c71f93
Javier Rojas <jrojas03.08.98@gmail.com>
2020-02-06 01:37:40 -0400

    Created btn and implemented working publisher.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './publisher.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,6 +21,14 @@ class _HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<_HomePage> {
+  bool isAvailableBtn;
+
+  @override
+  void initState() {
+    isAvailableBtn = true; //Change to false when sub is correctly implemented
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +36,31 @@ class _HomePageState extends State<_HomePage> {
         title: Text('Quiz Up!'),
         backgroundColor: Colors.green[600],
       ),
+      body: Center(
+        child: SizedBox(
+          height: (MediaQuery.of(context).size.height / 2),
+          width: double.infinity,
+          child: FloatingActionButton(
+            onPressed: isAvailableBtn ?
+                () => {PublishAnswer('Team A '), _DeactivateBtn()}
+                : null,
+            backgroundColor: Colors.deepOrange,
+            child: Icon(Icons.block),
+          ),
+        ),
+      ),
     );
+  }
+
+  void ActivateBtn() {
+    setState(() {
+      isAvailableBtn = true;
+    });
+  }
+
+  void _DeactivateBtn() {
+    setState(() {
+      isAvailableBtn = false;
+    });
   }
 }
